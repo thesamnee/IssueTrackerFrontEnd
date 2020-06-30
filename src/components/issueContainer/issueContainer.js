876ef3abcbc91d96259e5dc2a1a438f6d592d3d8
Sam Nee <samuel.r.nee@gmail.com>
2020-06-29 21:37:11 -0400

    set up basic JSX for issuecontainer

--- src/components/issueContainer/issueContainer.js
+++ src/components/issueContainer/issueContainer.js
@@ -1,6 +1,72 @@
-import React from 'react'
-import './issueContainer.css'
+import React, { Component } from "react";
+import Issue from "./issue.js";
+import "./issueContainer.css";
+import { Link } from "react-router-dom"
+import axios from 'axios';
 
-let issueContainer = () =>{
+class issueContainer extends Component {
+  state = {
+    issues: []
+  };
+  
+  componentDidMount(){
+    //Add Axios call to get issues from API
+  }
 
+  render() {
+    return (
+      <div className="issueSontainer">
+        <div className="issueStatus" status="new">
+          <h2 className="statusHeader">New</h2>
+          {this.state.issues
+            .filter(issues => issues.status === "new")
+            .map(issues => (
+              <Issue
+                status={issues.status}
+                title={issues.title}
+                desc={issues.desc}  
+              />
+            ))}
+        </div>
+        <div className="issueStatus" status="inProgress">
+          <h2 className="statusHeader">In Progress</h2>
+          {this.state.issues
+            .filter(issues => issues.status === "inProgress")
+            .map(issues => (
+              <Issue
+              status={issues.status}
+              title={issues.title}
+              desc={issues.desc}
+              />
+              ))}
+        </div>
+        <div className="issueStatus" status="complete">
+          <h2 className="statusHeader">Complete</h2>
+          {this.state.issues
+            .filter(issues => issues.status === "complete")
+            .map(issues => (
+              <Issue
+                status={issues.status}
+                title={issues.title}
+                desc={issues.desc}
+                />
+                ))}
+        </div>
+        <div className="issueStatus" status="noAction">
+          <h2 className="statusHeader">No Action Needed</h2>
+          {this.state.issues
+            .filter(issues => issues.status === "noAction")
+            .map(issues => (
+              <Issue
+              status={issues.status}
+              title={issues.title}
+              desc={issues.desc}
+                />
+                ))}
+        </div>
+        </div>
+    );
+  }
 }
+
+export default issueContainer;
