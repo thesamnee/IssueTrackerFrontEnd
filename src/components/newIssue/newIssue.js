07cd1343994ad09f3783eb5d624ffa4e340b01d8
Sam Nee <samuel.r.nee@gmail.com>
2020-07-28 12:07:57 -0400

    added some functions to newIssue

--- src/components/newIssue/newIssue.js
+++ src/components/newIssue/newIssue.js
@@ -13,6 +13,54 @@ class newIssue extends Component {
         });
         console.log(this.state.title)
       };
+    onChangeIssueDesc = e => {
+        this.setState({
+            title: e.target.value
+        });
+        console.log(this.state.desc)
+    };
+    onChangeIssueStatus = e => {
+        this.setState({
+            status: e.target.value
+        });
+        console.log(this.state.desc)
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+    
+        console.log(`Submitted`);
+        console.log(`Title: ${this.state.title}`);
+        console.log(`Category: ${this.state.category}`);
+        console.log(`Description: ${this.state.description}`);
+        console.log(`Time: ${this.state.time}`);
+        console.log(`Duration: ${this.state.duration}`);
+    
+        const newTodo = {
+          day: this.state.day,
+          title: this.state.title,
+          category: this.state.category,
+          duration: this.state.duration,
+          description: this.state.description,
+          time: this.state.time
+        };
+    
+        axios
+          .post("http://localhost:4000/todos/add", newTodo)
+          .then(res => {
+            this.setState({
+              day: "",
+              title: "",
+              category: "",
+              description: "",
+              time: Number,
+              duration: Number
+            });
+            this.props.history.push("/schedule")
+          });
+    
+        
+      };
 
     render() {
         return (
